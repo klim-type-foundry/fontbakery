4a46e21c2434fb24b7ce8d03b5dfcf6ba6c93bf4
Peter Dekkers <425971+PeterDekkers@users.noreply.github.com>
2023-06-07 12:51:46 +1200

    Read Vue report from file

--- Lib/fontbakery/reporters/html_klim.py
+++ Lib/fontbakery/reporters/html_klim.py
@@ -26,29 +26,33 @@ class KlimHtmlReporter(HTMLReporter):
         print(f'A report in HTML format has been saved to "{self.output_file}"')
 
     def get_html(self) -> str:
-        """Return complete report as a HTML string."""
-
-        return f"""
-        <script type="importmap">
-          {{
-            "imports": {{
-              "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser{'.prod' if PRODUCTION_JS else ''}.js"
-            }}
-          }}
-        </script>
-
-        <div id="app">{{{{ message }}}}<br /><br /><pre>{{{{ checkData }}}}</pre></div>
-
-        <script type="module">
-          import {{ createApp }} from 'vue';
-
-          createApp({{
-            data() {{
-              return {{
-                message: `Hello Font Bakery!`,
-                checkData: {json.dumps(self.getdoc(), sort_keys=True)}
-              }}
-            }}
-          }}).mount('#app');
-        </script>
-        """
+        with open("../../../frontend/vue-report.html") as f:
+            return f"""
+              <script>const checkData = {json.dumps(self.getdoc(), sort_keys=True)};</script>
+              {f.read()}
+            """
+        #
+        # return f"""
+        # <script type="importmap">
+        #   {{
+        #     "imports": {{
+        #       "vue": "https://unpkg.com/vue@3/dist/vue.esm-browser{'.prod' if PRODUCTION_JS else ''}.js"
+        #     }}
+        #   }}
+        # </script>
+        #
+        # <div id="app">{{{{ message }}}}<br /><br /><pre>{{{{ checkData }}}}</pre></div>
+        #
+        # <script type="module">
+        #   import {{ createApp }} from 'vue';
+        #
+        #   createApp({{
+        #     data() {{
+        #       return {{
+        #         message: `Hello Font Bakery!`,
+        #         checkData: {json.dumps(self.getdoc(), sort_keys=True)}
+        #       }}
+        #     }}
+        #   }}).mount('#app');
+        # </script>
+        # """
