84f12036bd7e93bc19ee15ae21b1e925f7a533b7
Peter Dekkers <425971+PeterDekkers@users.noreply.github.com>
2024-01-30 16:00:39 +1300

    Adjust section key formatting

--- Lib/fontbakery/reporters/html_app_frontend/src/utils/formatFontBakeryKey.ts
+++ Lib/fontbakery/reporters/html_app_frontend/src/utils/formatFontBakeryKey.ts
@@ -1,4 +1,5 @@
 export default function formatFontBakeryKey(key: FontBakeryKey) {
+    return key;
     const formattedKey = key[0].replace('<Section: ', '');
     return formattedKey.substring(0, formattedKey.length - 1);
 }
